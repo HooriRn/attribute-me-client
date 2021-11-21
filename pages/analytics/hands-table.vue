ebf9c018196c16819ad0b3b189b8706e50ae3707
Amin AzGol <golestani76@yahoo.com>
2021-11-21 12:36:34 +0330

    add hostname col and align ltr last 2 cols

--- pages/analytics/hands-table.vue
+++ pages/analytics/hands-table.vue
@@ -1,14 +1,14 @@
 <template>
   <hot-table v-if="tableData.length > 0" :settings="settings" :data="tableData" ref="hotTable">
-      <hot-column  v-if="tableData[0].date !== undefined" title="Date" read-only="true" data="date" :settings="{className: 'notInvalid', type: 'date', dateFormat: 'DD MMM YYYY'}">
+      <hot-column  v-if="tableData[0].date !== undefined" title="Date" read-only="true" data="date" :settings="{type: 'date', dateFormat: 'DD MMM YYYY'}">
       </hot-column>
-      <hot-column v-if="tableData[0].time !== undefined" title="Date & Hour" read-only="true" data="time" :settings="{className: 'notInvalid', type: 'date', dateFormat: 'DD MMM YYYY, hh:mm A'}">
+      <hot-column v-if="tableData[0].time !== undefined" title="Date & Hour" read-only="true" data="time" :settings="{type: 'date', dateFormat: 'DD MMM YYYY, hh:mm A'}">
       </hot-column>
-      <hot-column title="Event Value" read-only="true" data="event_value" :settings="{className: 'htLeft', type: 'numeric', numericFormat: {pattern: '0,0'}}">
+      <hot-column title="Event Value" read-only="true" data="event_value" :settings="{type: 'numeric', numericFormat: {pattern: '0,0.00'}}">
       </hot-column>
-      <hot-column title="Event Count" read-only="true" data="event_count" :settings="{className: 'htLeft', type: 'numeric', numericFormat: {pattern: '0,0'}}">
+      <hot-column title="Event Count" read-only="true" data="event_count" :settings="{type: 'numeric', numericFormat: {pattern: '0,0'}}">
       </hot-column>
-      <hot-column title="Event Label" read-only="true" data="event_label">
+      <hot-column title="Event label" read-only="true" data="event_label">
       </hot-column>
       <hot-column title="Event Category" read-only="true" data="event_category">
       </hot-column>
@@ -22,6 +22,8 @@
       </hot-column>
       <hot-column title="Device" read-only="true" data="device_category">
       </hot-column>
+      <hot-column title="Hostname" read-only="true" data="host_name">
+      </hot-column>
       <hot-column title="Page Path" read-only="true" data="page_path">
       </hot-column>
       <hot-column title="Page Referrer" read-only="true" data="page_referrer">
@@ -34,8 +36,7 @@ import { mapGetters } from 'vuex'
 
 export default {
   computed: {
-    ...mapGetters(['exportCSV']),
-    ...mapGetters(["tableSetting"])
+  ...mapGetters(['exportCSV'])
   },
   props: {
     tableData: {
@@ -94,7 +95,9 @@ export default {
             sortOrder: 'desc'
           }
         },
-        autoColumnSize: true,
+        cell: [
+          {row: 0, col: 1, className: 'htRight'},
+        ],
         // mergeCells: [
         //   { row: 0, col: 1, rowspan: 1, colspan: 10 },
         // ],
@@ -102,6 +105,10 @@ export default {
         persistentState: true,
         cells: function(row, col, prop) {
           const cellProperties = {};
+
+          // if (col >= 11) {
+          //   cellProperties.className = 'htRight';
+          // }
 
           if (col == 0) {
             cellProperties.renderer = function (instance, td, row, col, prop, value, cellProperties) {
@@ -115,6 +122,9 @@ export default {
         },
         afterGetColHeader: function(col, th) {
           th.className = 'htLeft'
+          // if (col >= 11) {
+          //   th.className = 'htRight';
+          // }
         }
         // renderer(instance, td, row, col, prop, value, cellProperties) {
         //     const escaped = Handsontable.helper.stringify(value);
@@ -138,19 +148,6 @@ export default {
     const hotTableEl = this.$refs['hotTable']
     if(!hotTableEl) return
     const hot = this.$refs['hotTable'].hotInstance
-
-    if (this.tableSetting &&
-    (
-      this.tableSetting['filter_website'] == 'THORChain.org' ||
-      this.tableSetting['present_filter'] == 'New Wallets' ||
-      this.tableSetting['present_filter'] == 'Interface Loads'
-    )) {
-      hot.updateSettings({
-        hiddenColumns: {
-          columns: [1],
-        }
-      })
-    }
 
     self.addTotalRow(hot)
 
@@ -179,17 +176,29 @@ export default {
   methods:{
     addTotalRow(hotTable){
       console.log('add total row')
-      let event_total_value = hotTable.getData().reduce((a, b) => a+parseFloat((b[1]??'0').toString().replace(/\,/g, '')), 0).toFixed(2)
+      let event_total_value = hotTable.getData().reduce((a, b) => a+parseFloat((b[1 ]??'0').toString().replace(/\,/g, '')), 0).toFixed(2)
       let event_count = hotTable.getData().reduce((a, b) => a+parseFloat((b[2]??'0').toString().replace(/\,/g, '')), 0).toFixed(2)
       hotTable.alter('insert_row', 0 , 1)
+      // var mergeCells = hotTable.getPlugin('mergeCells')
 
+      // hotTable.updateSettings({
+      //   fixedRowsTop: 10
+      // })
+      // hotTable.render();
+
+      // mergeCells.merge(0, 0, 0, 6)
       hotTable.setDataAtCell(0, 0, 'Totals');
       hotTable.setDataAtCell(0, 1, event_total_value);
       hotTable.setDataAtCell(0, 2, event_count);
       hotTable.updateSettings({
+        cell: [
+          {row: 0, col: 1, className: 'htRight'},
+        ],
         fixedRowsTop: 1
       });
 
+      // hotTable.setDataAtCell(0, 9, this.totals.total_total_users);
+      // hotTable.setDataAtCell(0, 10, this.totals.total_event_count_per_user);
     },
     removeTotalRow(hotTable){
       console.log('remove total row')
